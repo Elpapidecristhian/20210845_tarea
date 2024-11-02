cf416cb31b0a76e2fd9dba793be16867f3f05123
Elpapidecristihan <alejandrogutarra5@gmail.com>
2024-11-01 21:34:55 -0500

    Bienvenido Alejandro

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("¡Proyecto de java para el github");
+        System.out.println("Bienvenido, Alejandro”");
     }
 }
